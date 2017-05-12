90eb93d4966167b60d46323d9f672b1d4c3a8b38
ıqO <merenuou@gmail.com>
2017-05-12 13:13:08 -0600

    remove crap

--- wp-content/plugins/owl-utility/tests/test-strathcom-cleaner-public.php
+++ wp-content/plugins/owl-utility/tests/test-strathcom-cleaner-public.php
@@ -6,56 +6,52 @@
  */
 
 /**
- * 
+ *
  */
 class Strathcom_Cleaner_Public_Test extends WP_UnitTestCase {
 
 	/**
-	 * 
+	 *
 	 */
-	public function test_sample() { 
-		$class = new Strathcom_Cleaner_Public();
+	public function test_sample() {
 
-error_log($class);
-		// $this->assertTrue( true );
+	    // boilerplate WIP for tester.
+			// $class = new Strathcom_Cleaner_Public();.
+			// $class->strat_ob_class_slug();.
+			//
+			// $this->assertTrue( true );.
 	}
-
 
 	public function test_enqueue_styles() {
 
 	}
 
-    public function test_strat_cleanup() {
+	public function test_strat_cleanup() {
 
-    }   
-    
-    public function test_strat_remove_x_pingback() {
-     
-    }
+	}
 
-    public function test_strat_remove_comments_inline_styles() {
+	public function test_strat_remove_x_pingback() {
 
-    }
+	}
 
-    public function test_strat_remove_gallery_styles() {
-      
-    }
+	public function test_strat_remove_comments_inline_styles() {
 
-    public function test_strat_ob_class_slug() {
-    	$class = new Strathcom_Cleaner_Public();
-		$class->strat_ob_class_slug();
-		
-		$this->assertTrue( true );
-       
-    }
-	
+	}
+
+	public function test_strat_remove_gallery_styles() {
+
+	}
+
+	public function test_strat_ob_class_slug() {
+
+	}
+
 	public function test_dequeue_styles() {
-     
+
 	}
 
 	public function test_dequeue_scripts() {
-       
+
 	}
 
-   
 }
