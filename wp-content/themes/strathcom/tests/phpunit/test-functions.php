d5e20fe52a6ad2521dd095eb83d8549b206d5acf
whatwecode <merenuou@yahoo.com>
2017-05-23 05:38:09 +0000

    deploy test

--- wp-content/themes/strathcom/tests/phpunit/test-functions.php
+++ wp-content/themes/strathcom/tests/phpunit/test-functions.php
@@ -2,9 +2,9 @@
 /**
  * Unit Tests for Functions.php.
  *
- * @author XWP
+ * @author X
  * @package WordPress
- * @subpackage StrathcomCMS
+ * @subpackage W
  */
 
 /**
@@ -12,36 +12,4 @@
  */
 class Test_Functions extends WP_UnitTestCase {
 
-	/**
-	 * Test Phone Formatting.
-	 *
-	 * Tests phone number formats to ensure the
-	 * function responds properly, as either
-	 * a string starting with with "tel:+",
-	 * else an empty string.
-	 */
-	function test_format_phone_as_url() {
-		$good_numbers = array(
-			'1234567890',
-			'8254849987',
-			937627546,
-			'123-456-7890',
-		);
-
-		foreach ( $good_numbers as $good_number ) {
-			$result = format_phone_as_url( $good_number );
-			$this->assertStringStartsWith( 'tel:+', $result );
-		}
-
-		$bad_numbers = array(
-			'abcdefghi#$%',
-			'987-09a-9256',
-			'832a937392',
-		);
-
-		foreach ( $bad_numbers as $bad_number ) {
-			$result = format_phone_as_url( $bad_number );
-			$this->assertEquals( '', $result );
-		}
-	}
 }
