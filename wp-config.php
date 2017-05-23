10062cda2f4d5b7078f6cf13a735770d5695726f
whatwecode <merenuou@yahoo.com>
2017-05-23 04:59:01 +0000

    setup

--- wp-config.php
+++ wp-config.php
@@ -148,6 +148,17 @@ if ( ! defined( 'WP_DEBUG' ) ) {
     define('WP_DEBUG', false);
 }
 
+
+
+define( 'WP_ALLOW_MULTISITE', true );
+define( 'MULTISITE', true );
+define( 'SUBDOMAIN_INSTALL', false );
+$base = '/';
+define( 'DOMAIN_CURRENT_SITE', 'otestsite.local' );
+define( 'PATH_CURRENT_SITE', '/' );
+define( 'SITE_ID_CURRENT_SITE', 1 );
+define( 'BLOG_ID_CURRENT_SITE', 1 );
+
 /* That's all, stop editing! Happy Pressing. */
 
 
