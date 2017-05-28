f12875e3ecabc729958c8713dc0b555e61a582a9
whatwecode <merenuou@yahoo.com>
2017-05-28 01:39:02 +0000

    test change

--- dev-scripts/builder.sh
+++ dev-scripts/builder.sh
@@ -18,6 +18,7 @@ ls -al
 # npm install
 
 
+
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
