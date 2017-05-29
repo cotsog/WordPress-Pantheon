a58aa68ef19d5585ff0b78ea225ab6a0e99901ef
CI <merenuou@yahoomail.com>
2017-05-29 16:48:38 +0000

    push changes

--- dev-scripts/deploy.sh
+++ dev-scripts/deploy.sh
@@ -13,11 +13,18 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+# if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+#     echo ""
+#     exit 0
+# fi
 # Create a new empty branch if it doesn't exist yet
 git checkout $SOURCE_BRANCH
 git pull
 git fetch -p
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+git pull
+git fetch -p
 git merge $SOURCE_BRANCH
 git config user.name "CI"
 git config user.email "merenuou@yahoomail.com"
@@ -32,6 +39,7 @@ fi
 # The delta will show diffs between new and old versions.
 git add .
 git commit -m "Deploy to branch: ${SHA}"
+
 
 # Get the deploy key by using Travis's stored variables to decrypt github_deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
